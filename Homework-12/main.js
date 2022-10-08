9e36c7ace74ab3aff9c269eab58dd4d2f06617c0
Serhii <sergii.ippolitov@gmail.com>
2022-10-08 22:01:14 +0300

    added HW#12

--- Homework-12/main.js
+++ Homework-12/main.js
@@ -1,43 +1,97 @@
+const urlFilms = "https://swapi.dev/api/films";
+let urlPlanets = "https://swapi.dev/api/planets/";
+const buttonGetInfo = document.querySelector(".heroes");
+const buttonGetPlanets = document.querySelector(".stars");
+const inputGetFilm = document.querySelector(".film");
+
 async function getInfo() {
+  document.querySelector(
+    ".content"
+  ).innerHTML = `<p class="loading">Loading...</p>`;
   try {
-    let characters = await fetch("https://swapi.dev/api/people/");
-    let arrayOfCharacters = await characters.json();
-    arrayOfCharacters = arrayOfCharacters.results;
-    let person;
-    let list = document.querySelector(".characters");
-    for (person in arrayOfCharacters) {
-      list.innerHTML += `
-        <li class="person">
-            <h3>${arrayOfCharacters[person].name}</h3>
-            <span>${arrayOfCharacters[person].birth_year}</span>
-            <a href="">${arrayOfCharacters[person].gender}</a>
-        </li>
-         `;
+    let request = await fetch(`${urlFilms}/${inputGetFilm.value}`, {
+      method: "GET",
+    });
+    let response = await request.json();
+    let heroesRequest = [];
+    let heroesResponse = [];
+    for (let i = 0; i < response.characters.length; i++) {
+      heroesRequest[i] = await fetch(response.characters[i]);
+      heroesResponse[i] = await heroesRequest[i].json();
     }
-  } catch {
-    alert(err);
+    showInfo(heroesResponse);
+  } catch (error) {
+    alert("Будь ласка виберіть фільм.");
   }
 }
-
-let heroes = document.querySelector(".heroes");
-heroes.onclick = getInfo;
 
 async function getPlanets() {
+  document.querySelector(".content").innerHTML = "";
+  try {
+    let request = await fetch(urlPlanets, {
+      method: "GET",
+    });
+    let response = await request.json();
+    showPlanets(response.results);
+    let buttonPrev = createButton("planetsButtons", "< prev");
+    let buttonNext = createButton("planetsButtons", "next >");
+    
+    if(!response.previous == 0) {
+      buttonPrev.addEventListener("click", function() {
+        urlPlanets = response.previous;
+        getPlanets();
+      });
+    };
+    if(!response.next == 0) {
+      buttonNext.addEventListener("click", function() {
+        urlPlanets = response.next;
+        getPlanets();
+      })
+    }
+  } catch(error) {
+    alert(error);
+  }
+}
+function showInfo(array) {
+  document.querySelector(".content").innerHTML = "";
+  array.forEach((element) => {
+    createList("info").innerHTML =
+    `${element.name}  <img class="img" src = ./img/${getGender(element.gender)} width ="20px"> ${element.birth_year}
+    `
+  });
+}
 
-  let planets = await fetch("https://swapi.dev/api/planets/");
-  let planetsSW = await planets.json();
-  planetsSW = planetsSW.results;
-  console.log(planetsSW);
-  let planet;
-  let list = document.querySelector(".planets");
-  for(planet in planetsSW) {
-    list.innerHTML += `
-    <li class="planets">
-        <h3>${planetsSW[planet].name}</h3>
-    </li>
-     `;
+function showPlanets(array) {
+  document.querySelector(".content").innerHTML = "";
+  array.forEach(element => {
+    createList("planets").innerHTML = element.name;
+  })
+}
+
+function getGender(gender) {
+  if(gender === "male") {
+    return "male.svg"
+  } else if(gender === "female") {
+    return "female.svg"
+  } else {
+    return "robot.svg"
   }
 }
 
-let planets = document.querySelector(".stars");
-planets.onclick = getPlanets;
+function createList(className) {
+  let div = document.createElement("div");
+  document.querySelector(".content").append(div);
+  div.classList.add(className);
+  return div;
+}
+
+function createButton(className, html) {
+  let button = document.createElement("button");
+  document.querySelector(".content").append(button);
+  button.classList.add(className);
+  button.innerHTML = html;
+  return button;
+}
+
+buttonGetInfo.onclick = getInfo;
+buttonGetPlanets.onclick = getPlanets;
--- Homework-13/main.js
+++ Homework-13/main.js
@@ -1,37 +1,38 @@
 function* createIdGenerator() {
-    let count = 1;
-    while(true) {
-      yield count++;
-    }
+  let count = 1;
+  while (true) {
+    yield count++;
   }
-  const idGenerator = createIdGenerator();
-  
-  console.log(idGenerator.next().value);
-  console.log(idGenerator.next().value);
-  console.log(idGenerator.next().value);
-  console.log(idGenerator.next().value);
+}
+const idGenerator = createIdGenerator();
+
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
 
 /*******************************/
 
 const increase = document.querySelector(".increase");
 const reduce = document.querySelector(".reduce");
-const text = document.querySelector(".text");
 
 function* newFontGenerator(font) {
-    let changeFont = yield;
-    while(true) {
-        changeFont === "increase" ? font +=2 : font -= 2;
-        changeFont = yield font;
-    }
-}
+  let changeFont = yield;
+  while (true) {
+    changeFont === "increase" ? font += 2 : font -= 2;
+    changeFont = yield font;
+  };
+};
 
-const fontGenerator = newFontGenerator(14);
+const fontGenerator = newFontGenerator(16);
 
 fontGenerator.next();
 
+
 increase.addEventListener("click", () => {
-    text.style.fontSize = `${fontGenerator.next("increase").value}px`
-})
+  document.body.style.fontSize = `${fontGenerator.next("increase").value}px`;
+});
+
 reduce.addEventListener("click", () => {
-    text.style.fontSize = `${fontGenerator.next("reduce").value}px`
-})
+  document.body.style.fontSize = `${fontGenerator.next("reduce").value}px`;
+});
